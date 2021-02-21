22120663fbbbfe92c7259ae549a694f1fdcfe8f8
tamanapplied <tamanapplied@gmail.com>
2021-02-20 20:50:32 -0600

    fix

--- flaskex.py
+++ flaskex.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
-from db import db
 
 from security import auth, identity
 from resources.user import UserRegister
@@ -14,10 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, auth, identity)
 
